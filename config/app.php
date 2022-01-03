3a3c0ac01778dfef2ff0023430d5575de734c7ed
holrilo <senky98@gmail.com>
2022-01-03 11:41:53 -0500

    cargar a github

--- config/app.php
+++ config/app.php
@@ -175,6 +175,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /* 
+         * spatie/laravel-permission 
+         */
+        Spatie\Permission\PermissionServiceProvider::class,
+
     ],
 
     /*
